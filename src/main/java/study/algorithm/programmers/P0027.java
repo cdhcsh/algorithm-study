ea6ebb44eebf9b942b2e97e1889f1c71a514922c
cdhcsh <cdhcsh@gmail.com>
2022-12-21 23:02:30 +0900

    12/21 크레인 인형뽑기 게임

---
+++ src/main/java/study/algorithm/programmers/P0027.java
@@ -0,0 +1,33 @@
+package study.algorithm.programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class P0027 {
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Queue<Integer>[] board_q = new Queue[board[0].length];
+
+        Stack<Integer> pocket = new Stack<>();
+        for(int i = 0 ; i < board_q.length ; i++){
+            board_q[i] = new LinkedList<>();
+            for(int j = 0 ; j < board.length ; j++){
+                System.out.print(board[j][i]+" ");
+                if(board[j][i]>0)board_q[i].add(board[j][i]);
+            }
+            System.out.println();
+        }
+        for (int i : moves) {
+            if(board_q[i-1].isEmpty()) continue;
+            int n = board_q[i-1].remove();
+            if(!pocket.isEmpty() && pocket.peek()==n){
+                pocket.pop();
+                answer+=2;
+            }
+            else
+                pocket.push(n);
+        }
+        return answer;
+    }
+}
--- src/main/java/study/algorithm/start.java
+++ src/main/java/study/algorithm/start.java
@@ -8,6 +8,7 @@ public class start {
 //        System.out.println(P0007.solution2(new int[]{1,2,1,2,3,1,3,1}));
 //        System.out.println(P0008.solution(new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"}));
 //        System.out.println(P0011.solution("10000","20300045"));
-        System.out.println(P0026.solution(new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},"right"));
+//        System.out.println(P0026.solution(new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},"right"));
+        System.out.println(P0027.solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}},new int[]{1,5,3,5,1,2,1,4}));
     }
 }
