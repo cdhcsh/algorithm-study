81f930fbbf980871401a0fa6327cbd8d9cb1382d
cdhcsh <cdhcsh@gmail.com>
2023-01-12 23:07:27 +0900

    1/12 숫자의 표현

---
+++ src/main/java/study/algorithm/programmers/P0065.java
@@ -0,0 +1,13 @@
+package study.algorithm.programmers;
+
+public class P0065 {
+    public int solution(int n) {
+        int cnt = 0;
+        int i = 1;
+        while(n>0){
+            if(n%i==0)cnt++;
+            n-=i++;
+        }
+        return cnt;
+    }
+}
--- src/main/java/study/algorithm/start.java
+++ src/main/java/study/algorithm/start.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public class start {
     public static void main(String[] args) {
-        System.out.println(new P0061().solution(6));
+        System.out.println(new P0065().solution(15));
     }
 }
