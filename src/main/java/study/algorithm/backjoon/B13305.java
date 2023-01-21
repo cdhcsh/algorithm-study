35839ee66c01997b408a77401504b7b0770d1450
최동환 <cdhcsh@gmail.com>
2023-01-21 19:36:47 +0900

    1/21 주유소

---
+++ src/main/java/study/algorithm/backjoon/B13305.java
@@ -0,0 +1,39 @@
+package study.algorithm.backjoon;
+
+/**
+ * 주유소
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B13305 {
+    public static void main(String[] args) throws IOException {
+        //input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int length = Integer.parseInt(st.nextToken());
+        int[] cites = new int[length];
+        int[] distances = new int[length-1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < length-1 ; i++){
+            distances[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < length ; i++){
+            cites[i] = Integer.parseInt(st.nextToken());
+        }
+        //logic
+        long answer = 0;
+        long price = 1000000001;
+        for (int i = 0; i < length-1; i++) {
+            price = Math.min(price,cites[i]);
+            answer += price*distances[i];
+        }
+        //output
+        System.out.println(answer);
+
+    }
+}
