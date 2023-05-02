985d1b8b87683bd8e4350c33377522f8cb6618de
cdhcsh <cdhcsh@gmail.com>
2023-05-02 21:49:49 +0900

    5/2 가장 큰 증가하는 부분 수열

---
+++ src/main/java/study/algorithm/B11055.java
@@ -0,0 +1,41 @@
+package study.algorithm;
+
+/**
+ * 가장 큰 증가하는 부분 수열
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        int answer = arr[0];
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[j] + arr[i],dp[i]);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer);
+
+
+
+    }
+}
--- src/main/java/study/algorithm/backjoon/B01463.java
+++ src/main/java/study/algorithm/backjoon/B01463.java
@@ -5,16 +5,18 @@ package study.algorithm.backjoon;
  */
 
 import java.io.*;
+import java.util.Arrays;
 
 public class B01463 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-        dp[n] = 0;
-        for (int i = n; i > 1; i--) {
+        int X = Integer.parseInt(br.readLine());
+        int[] dp = new int[X+1];
+        Arrays.fill(dp,X);
+        dp[X] = 0;
+        for (int i = X; i > 1; i--) {
             if(i%2==0) update(dp,i,i/2);
             if(i%3==0) update(dp,i,i/3);
             update(dp,i,i-1);
@@ -23,6 +25,6 @@ public class B01463 {
         bw.close();
     }
     static void update(int[] arr,int from, int to){
-        arr[to] = arr[to] == 0 ? arr[from]+1 : Math.min(arr[from]+1,arr[to]);
+        arr[to] = Math.min(arr[from]+1,arr[to]);
     }
 }
