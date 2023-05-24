12316cb557e03485c46c4b15682d07fffb1aa448
cdhcsh <cdhcsh@gmail.com>
2023-05-25 04:31:38 +0900

    5/24 보석도둑,중력큐

--- src/main/java/study/algorithm/B01202.java
+++ src/main/java/study/algorithm/B01202.java
@@ -16,13 +16,12 @@ public class B01202 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         bag = new int[K];
-        PriorityQueue<Integer[]> queue = new PriorityQueue<>((i1, i2) -> {
+        PriorityQueue<Integer[]> gemQueue = new PriorityQueue<>((i1, i2) -> {
             if (i1[0].equals(i2[0])) return i2[1]-i1[1];
             else return i1[0]-i2[0];
         });
@@ -31,7 +30,7 @@ public class B01202 {
             Integer[] gem = new Integer[2];
             gem[0] = Integer.valueOf(st.nextToken());
             gem[1] = Integer.valueOf(st.nextToken());
-            queue.offer(gem);
+            gemQueue.offer(gem);
         }
         for (int i = 0; i < K; i++) {
             bag[i] = Integer.parseInt(br.readLine());
@@ -40,10 +39,10 @@ public class B01202 {
 
         PriorityQueue<Integer> temp = new PriorityQueue<>((i1, i2) -> i2-i1);
 
-        int answer = 0;
+        long answer = 0;
         for (int i = 0; i < K; i++) {
-            while(!queue.isEmpty() && queue.peek()[0] <= bag[i]){
-                temp.offer(queue.poll()[1]);
+            while(!gemQueue.isEmpty() && gemQueue.peek()[0] <= bag[i]){
+                temp.offer(gemQueue.poll()[1]);
             }
             if(!temp.isEmpty())answer+=temp.poll();
         }
--- src/main/java/study/algorithm/B28078.java
+++ src/main/java/study/algorithm/B28078.java
@@ -1,59 +1,61 @@
 package study.algorithm;
 
-import study.algorithm.backjoon.B01021;
+/**
+ * 중력 큐
+ */
 
 import java.io.*;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class B28078 {
-    static Deque<Integer> deque;
-    static Node front = new Node(-1);
+    static LinkedList<Integer> deque = new LinkedList() {
+    };
+    static int mode = 0;
+    /*
+    0 -> 뒤 - 앞 ..1,2,3 시계방향회전
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
-            int mode = 0;
             if(command.equals("push")){
-
+                String sub = st.nextToken();
+                if(sub.equals("b"))deque.add(0);
+                else deque.add(1);
+                rotating();
             }else if(command.equals("rotate")){
-
+                String sub = st.nextToken();
+                if(sub.equals("r")) mode = (mode+1)%4;
+                else mode = (mode+3)%4;
+                rotating();
             } else if (command.equals("count")) {
-
+                String sub = st.nextToken();
+                if(sub.equals("b")) bw.write(count(0)+"\n");
+                else bw.write(count(1)+"\n");
             } else if (command.equals("pop")) {
-
+                deque.poll();
+                rotating();
             }
         }
-
-
+        bw.close();
     }
-    static class Node{
-        Node pre = null;
-        Node next = null;
-        int value;
-        public boolean hasNextWall(){
-            if(value==1) return true;
-            else if(this.next!=null) return this.next.hasNextWall();
-            else return false;
+    public static void rotating(){
+        if(mode == 1){
+            while(!deque.isEmpty() && deque.peek()==0){
+                deque.remove();
+            }
         }
-        public boolean hasPreWall(){
-            if(value==1) return true;
-            else if(this.pre!=null) return this.pre.hasPreWall();
-            else return false;
+        else if (mode == 3){
+            while(!deque.isEmpty() && deque.peekLast()==0){
+                deque.removeLast();
+            }
         }
-        public Node(int value){
-            this.value = value;
-        }
-        public Node connect(Node node){
-            this.next = node;
-            node.pre = this;
-            return node;
-        }
-        public Node remove(){
-            return pre.connect(next);
-        }
+    }
+    public static int count(int c){
+        return (int) deque.stream().filter(i->i==c).count();
     }
 }
