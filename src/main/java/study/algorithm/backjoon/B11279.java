355c251c1e3b66cdd37735ec4c21bcc26db2dea8
cdhcsh <cdhcsh@gmail.com>
2023-06-12 23:37:45 +0900

    6/12 최대 힙

---
+++ src/main/java/study/algorithm/backjoon/B11279.java
@@ -0,0 +1,26 @@
+package study.algorithm.backjoon;
+
+/**
+ * 최대 힙
+ */
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class B11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>((i, j) -> Integer.compare(j, i));
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(br.readLine());
+            if (m == 0) {
+                if (queue.isEmpty()) bw.write("0\n");
+                else bw.write(queue.poll() + "\n");
+            } else queue.offer(m);
+        }
+        bw.close();
+    }
+}
