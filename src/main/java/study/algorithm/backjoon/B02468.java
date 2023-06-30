60b3e20d23f19dead84c7c917957f19c2eda2925
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2023-06-30 19:09:10 +0900

    6/30 꽃길,안전 영역

---
+++ src/main/java/study/algorithm/backjoon/B02468.java
@@ -0,0 +1,53 @@
+package study.algorithm.backjoon;
+
+/**
+ * 안전 영역
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B02468 {
+    static int N;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int answer = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        int max = 1;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(map[i][j], max);
+            }
+        }
+        for (int i = 1; i <= max; i++) {
+            int count = 0;
+            visit = new boolean[N][N];
+            for (int x = 0; x < N; x++) {
+                for (int y = 0; y < N; y++) {
+                    if(!visit[x][y] && map[x][y] > i) {
+                        dfs(x,y,i);
+                        count++;
+                    }
+                }
+            }
+            answer = Math.max(count, answer);
+        }
+        System.out.println(answer);
+    }
+    public static void dfs(int x,int y,int h){
+        if(x < 0 || x >= N || y < 0 || y >= N) return;
+        if(visit[x][y] || map[x][y] <= h) return;
+        visit[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            dfs(x+d[i][0],y+d[i][1],h);
+        }
+    }
+}
